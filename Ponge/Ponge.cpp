54c079b88b61209685ce76e494aeee2078304a31
Dock626 <oskarosmolak@gmail.com>
2026-02-08 00:06:08 +0100

    Add project files.

---
+++ Ponge/Ponge.cpp
@@ -0,0 +1,60 @@
+﻿// Pong.cpp : Defines the entry point for the application.
+//
+
+#include "Ponge.h"
+
+using namespace std;
+
+class Player {
+private:
+    Vector2 Position_;
+    float Speed_ = 5.0f;
+    float Current_Speed_;
+public:
+    Player(Vector2 Pos) {
+        Position_ = Pos;
+		Current_Speed_ = Speed_;
+    }
+
+    void Draw() {
+        DrawRectangle(Position_.x, Position_.y, 20, 80, BLUE);
+    }
+
+
+    float GetY() {
+        return Position_.y;
+    }
+
+    void Update() {
+        if (IsKeyDown(KEY_LEFT_SHIFT)) Current_Speed_ = Speed_ * 4;
+        else Current_Speed_ = Speed_;
+        if (IsKeyDown(KEY_UP)) Position_.y -= Current_Speed_;
+        if (IsKeyDown(KEY_DOWN)) { Position_.y += Current_Speed_; cout << Position_.y << "\n"; };
+    }
+};
+
+int main()
+{
+
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+    bool paused = false;
+    Player Test(Vector2(screenWidth / 2 - 400, screenHeight / 2));
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - input keys");
+    SetTargetFPS(60);
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        Test.Update();
+
+
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        Test.Draw();
+        EndDrawing();
+    }
+    return 0;
+}
---
+++ Ponge/Ponge.h
@@ -0,0 +1,9 @@
+﻿// Ponge.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include "raylib.h"
+
+// TODO: Reference additional headers your program requires here.
